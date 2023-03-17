b615366938c53fb1b2dcfc47f51f27a2368aa0c7
LiLk <khalil.abbioui@gmail.com>
2023-03-17 13:39:05 +0100

    Ajout des routes pour les nouvelles fonctionnalités back

--- TME_webAPI_DBO/mySearchEngine/mytig/urls.py
+++ TME_webAPI_DBO/mySearchEngine/mytig/urls.py
@@ -18,4 +18,8 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('update_stock/<int:pk>/', views.UpdateProductStock.as_view(), name='update_product_stock'),
     path('update_sale_percentage/<int:pk>/', views.UpdateProductSalePercentage.as_view(), name='update_product_sale_percentage'),
+    path('products_by_category/<str:category>/', views.ProductsByCategory.as_view(), name='products_by_category'),
+    path('update_multiple_product_stocks/', views.UpdateMultipleProductStocks.as_view(), name='update_multiple_product_stocks'),
+    path('update_multiple_product_promotions/', views.UpdateMultipleProductPromotions.as_view(), name='update_multiple_product_promotions'),
+
 ]
