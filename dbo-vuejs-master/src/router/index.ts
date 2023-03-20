50325096eb57d7e0274704de5b6cbd707fe8e1d5
Eric BARNET <70975601+EricBarnet@users.noreply.github.com>
2023-03-20 12:07:16 +0100

    Ajout page login

--- dbo-vuejs-master/src/router/index.ts
+++ dbo-vuejs-master/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ProductDetail from "../views/ProductDetail.vue";
+import LoginView from "../views/LoginView.vue";
+import TestView from "../views/TestView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -15,11 +17,48 @@ const routes: Array<RouteRecordRaw> = [
     component: ProductDetail,
     props: true,
   },
+
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
+  },
+
+  {
+    path: "/test",
+    name: "test",
+    component: TestView,
+    meta: {
+      requiresAuth: true,
+    },
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+// Ajoutez un guard de route pour la vérification de l'authentification
+router.beforeEach((to, from, next) => {
+  // Vérifiez si la route nécessite une authentification
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // Vérifiez si l'utilisateur a un jeton d'accès valide
+    const token = localStorage.getItem("token");
+    if (!token) {
+      // Si l'utilisateur n'a pas de jeton, redirigez-le vers la page de connexion
+      next({
+        path: "/login",
+        query: { redirect: to.fullPath },
+      });
+    } else {
+      // Si l'utilisateur a un jeton, laissez-le accéder à la route
+      next();
+    }
+  } else {
+    // Si la route n'a pas besoin d'authentification, laissez l'utilisateur y accéder
+    next();
+  }
 });
 
 export default router;
---
+++ dbo-vuejs-master/src/views/LoginView.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="login-container">
+    <div class="p-card login-card">
+      <h2 class="p-text-center">Connexion</h2>
+      <div class="p-fluid">
+        <div class="p-field p-mt-4">
+          <label for="username">Nom d'utilisateur</label>
+          <input
+            type="text"
+            id="username"
+            v-model="username"
+            class="p-inputtext p-component p-filled"
+          />
+        </div>
+        <div class="p-field p-mt-4">
+          <label for="password">Mot de passe</label>
+          <input
+            type="password"
+            id="password"
+            v-model="password"
+            class="p-inputtext p-component p-filled"
+          />
+        </div>
+        <div class="p-d-flex p-jc-between p-mt-6">
+          <div class="card flex justify-content-center">
+            <Button
+              type="button"
+              label="Se connecter"
+              icon="pi pi-user"
+              @click="login"
+            />
+          </div>
+          <div>
+            <button label="Link" class="p-button p-col-5" @click="clear">
+              Effacer
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: this.username,
+          password: this.password,
+        }),
+      };
+      fetch("http://localhost:8000/api/token/", requestOptions)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Nom d'utilisateur ou mot de passe incorrect.");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          // Stocker le jeton d'authentification dans le local storage pour une utilisation ultérieure
+          localStorage.setItem("token", data.access_token);
+
+          // Rediriger l'utilisateur vers la page d'accueil ou une autre page protégée
+          this.$router.push("/test");
+        })
+        .catch((error) => {
+          console.log(error);
+          alert(error.message);
+        });
+    },
+    clear() {
+      this.username = "";
+      this.password = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.login-container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-card {
+  width: 90%;
+  max-width: 400px;
+}
+</style>
